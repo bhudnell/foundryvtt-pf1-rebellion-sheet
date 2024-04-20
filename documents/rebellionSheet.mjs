2d44cc5b6f8bb57d503d6a2ca212ced225cc36c2
Brendon Hudnell <bthudnell@gmail.com>
2024-04-20 17:19:04 -0600

    added vertical scrolling

--- documents/rebellionSheet.mjs
+++ documents/rebellionSheet.mjs
@@ -17,7 +17,6 @@ import { getRankFromSupporters } from "../utils.mjs";
 // TODO track safehouses +1 security for each max +5
 // TODO add what week the rebellion is in to rebellion model/sheet
 // TODO add "advance week" button that clears out appropriate data (safehouses?, non persistent active events, anything else)
-// TODO add vertical scroll bars
 // TODO actions show what teams can do them ????
 // TODO add multiple recruiters
 /* TODO roll buttons:
