625c13d0ea53352151f39ada626bbefb7224ae56
Brendon Hudnell <bthudnell@gmail.com>
2024-04-25 14:33:59 -0600

    missed an import path

--- tools/extractPacks.mjs
+++ tools/extractPacks.mjs
@@ -3,7 +3,7 @@ import path from "path";
 
 import { extractPack } from "@foundryvtt/foundryvtt-cli";
 
-import { CFG } from "../config.mjs";
+import { CFG } from "../src/config.mjs";
 
 import { compiledPacksDir, sourcePacksDir } from "./config.mjs";
 import * as utils from "./utils.mjs";
